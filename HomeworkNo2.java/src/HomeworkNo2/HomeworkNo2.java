6bd32a60946921b82a2a6f2fd9edcd298a5f8af2
HomeworkNo2 <razo.w210@gmail.com>
2021-09-12 14:41:51 +0400

    HomeworkNo2 ֆիքսված

---
+++ HomeworkNo2.java/src/HomeworkNo2/HomeworkNo2.java
@@ -0,0 +1,41 @@
+package HomeworkNo2;
+
+public class HomeworkNo2 {
+
+    public static void main(String[] args) {
+    int a = 15;
+    int b = 12;
+    int c = b;
+
+    if (a > b) {
+      if (a > c) {
+          System.out.println(a);
+
+      }
+
+      }
+
+
+    int weekend = 3;
+     switch (weekend){
+         case 1 :
+             System.out.println("Friday");
+         case 2:
+             System.out.println("Saturday");
+         case 3:
+             System.out.println("Sunday");
+             break;
+         default:
+             System.out.println("Don't Worry");
+     }
+
+   int redIsHitFromBehind = 1;
+   int blackIsHitFromBehind = 2;
+   int
+
+   if redIsHitFromBehind
+
+
+    }
+    }
+
